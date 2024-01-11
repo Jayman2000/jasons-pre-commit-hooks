ed3c245cc513e44b5a131be219c2b670960e3d49
Jason Yundt <jason@jasonyundt.email>
2024-01-10 19:39:23 -0500

    Create command that verifies repo style

--- jasons_pre_commit_hooks/__init__.py
+++ jasons_pre_commit_hooks/__init__.py
@@ -2,12 +2,18 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
 import argparse
-import re
+import contextlib
 import io
+import os
+import pathlib
+import re
 import sys
 import warnings
 
+from collections.abc import Iterable
 from typing import Final
+
+import dulwich.repo
 
 
 # editorconfig-checker-disable
@@ -73,3 +79,36 @@ def forbid_paths() -> int:
                 )
                 exit_status = 1
     return exit_status
+
+
+def paths_in_repo() -> Iterable[pathlib.Path]:
+    # I would have used dulwich.porcelain.ls_files(), but that function
+    # isn’t typed.
+    ROOT: Final = str(pathlib.Path.cwd())
+    with contextlib.closing(dulwich.repo.Repo(ROOT)) as repo:
+        for byte_path in repo.open_index():
+            yield pathlib.Path(os.fsdecode(byte_path))
+
+
+def repo_style_checker() -> int:
+    make_stdout_stderr_handle_errors_better()
+    PARSER: Final = argparse.ArgumentParser(
+        description=(
+            "Makes sure that repos follow Jason’s style for repos."
+        )
+    )
+    # This command doesn’t take any arguments, so this just makes sure
+    # that --help works and that errors are produced if arguments are
+    # given.
+    PARSER.parse_args()
+
+    PATHS: Final = set(path for path in paths_in_repo())
+    COPYING: Final = pathlib.Path('copying.md')
+    if COPYING not in PATHS:
+        print(
+            f"ERROR: There’s no {COPYING} file.",
+            file=sys.stderr
+        )
+        return 1
+
+    return 0
