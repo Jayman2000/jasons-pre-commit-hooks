9ab773c9786f0bd3c4d60f26663ca501e3e9aa0b
Jason Yundt <jason@jasonyundt.email>
2024-01-16 08:55:47 -0500

    Add detect-bad-unicode command
    A future commit will turn this command into a pre-commit hook. It’s
    unlikely that these code points would ever be used in a file for a good
    purpose, so it makes sense to ban them. If they were to appear in a
    file, it would probably be because of a mistake. Additionally,
    detect-bad-unicode implicitly assumes that files use UTF-8 without a
    BOM, so it indirectly works to help enforce the use of UTF-8 without a
    BOM.

--- jasons_pre_commit_hooks/__init__.py
+++ jasons_pre_commit_hooks/__init__.py
@@ -4,11 +4,13 @@
 import argparse
 import contextlib
 import io
+import locale
 import os
 import pathlib
 import re
 import sys
 import textwrap
+import unicodedata
 import warnings
 
 from collections.abc import Iterable
@@ -16,6 +18,7 @@ from typing import Any, Final, NamedTuple, Optional
 
 import dulwich.repo
 import yaml
+import wcwidth
 
 
 # REUSE-IgnoreStart
@@ -213,6 +216,21 @@ PRE_COMMIT_REPOS_BY_PATH: Final = (
 
 
 # editorconfig-checker-disable
+def init() -> None:
+    """
+    Perform initialization common to all commands.
+
+    Every function that gets used as a console script [1] should start
+    by calling this function.
+
+    [1]: <https://setuptools.pypa.io/en/stable/userguide/entry_point.html#console-scripts>
+    """
+    # editorconfig-checker-enable
+    locale.setlocale(locale.LC_ALL, '')
+    make_stdout_stderr_handle_errors_better()
+
+
+# editorconfig-checker-disable
 def make_stdout_stderr_handle_errors_better() -> None:
     """
     Make this program work better on non-Unicode locales.
@@ -235,8 +253,85 @@ def make_stdout_stderr_handle_errors_better() -> None:
             )
 
 
+def line_and_maybe_column_number(
+    line: str,
+    line_index: int,
+    character_index: int
+) -> str:
+    return_value: str = f"line {line_index + 1:n}"
+    # When calculating the width, we purposely don’t include the
+    # character that we’re trying to find the position of. For whatever
+    # reason, U+34544 has East_Asian_Width set to “Wide” even though
+    # U+34544 isn’t assigned to any character [1]. In situations like
+    # that, it makes sense for the column number to point to the start
+    # of the character. Purposefully not including the character that
+    # we’re looking for while doing the width calculation allows us to
+    # point to the beginning of the character in that situation.
+    #
+    # [1]: <https://util.unicode.org/UnicodeJsps/character.jsp?a=34544>
+    width: int = wcwidth.wcswidth(line[:character_index])
+    if width >= 0:
+        return_value += f" column {width + 1:n}"
+
+    return return_value
+
+
+def detect_bad_unicode() -> int:
+    init()
+    PARSER: Final = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=(
+            "Checks to see if UTF-8 files contain “bad” code points.\n"
+            "\n"
+            "For the purposes of this command, a “bad” code point is a "
+            "surrogate code point, a private-use code point, a "
+            "noncharacter code point or a reserved code point. See "
+            "the Unicode Standard for details: <https://"
+            "www.unicode.org/versions/Unicode15.0.0/ch02.pdf#G14527>."
+        )
+    )
+    PARSER.add_argument(
+        'paths',
+        nargs='+',
+        type=pathlib.Path,
+        metavar="FILE"
+    )
+    ARGS: Final = PARSER.parse_args()
+
+    any_errors: bool = False
+    path: pathlib.Path
+    for path in ARGS.paths:
+        text: str = path.read_text(
+            encoding='utf_8',
+            errors='surrogatepass'
+        )
+
+        line_index: int
+        line: str
+        lines: Iterable[str] = text.splitlines(keepends=True)
+        for line_index, line in enumerate(lines):
+            character_index: int
+            character: str
+            for character_index, character in enumerate(line):
+                category: str = unicodedata.category(character)
+                if category in ('Cs', 'Co', 'Cn'):
+                    any_errors = True
+                    position: str = line_and_maybe_column_number(
+                        line,
+                        line_index,
+                        character_index
+                    )
+                    print(
+                        f"ERROR: {path}: {position}:",
+                        f"U+{ord(character):04X} is in a bad",
+                        f"Unicode general category: {category}",
+                        file=sys.stderr
+                    )
+    return any_errors
+
+
 def forbid_paths() -> int:
-    make_stdout_stderr_handle_errors_better()
+    init()
     PARSER: Final = argparse.ArgumentParser(
         description=(
             "Causes an error if paths match certain regex patterns."
@@ -440,7 +535,7 @@ def check_pc_config_hooks(
 
 
 def repo_style_checker() -> int:
-    make_stdout_stderr_handle_errors_better()
+    init()
     PARSER: Final = argparse.ArgumentParser(
         description=(
             "Makes sure that repos follow Jason’s style for repos."
---
+++ stubs/wcwidth.pyi
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
+
+
+def wcswidth(pwcs: str) -> int:
+    """
+    See wcwidth’s documentation.
+
+    <https://wcwidth.readthedocs.io/en/latest/api.html#wcwidth.wcswidth>
+    """
