51419b575d8a248264248e47e521dabe2616fdf7
Jason Yundt <jason@jasonyundt.email>
2025-05-09 11:04:28 -0400

    Make this repo’s flake JNFSG compliant

---
+++ flake-blueprint/checks/jnfsg-compliance-check.nix
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+{ inputs, ... }@blueprintArgs:
+inputs.jasons-nix-flake-style-guide.lib.mkComplianceCheck blueprintArgs
