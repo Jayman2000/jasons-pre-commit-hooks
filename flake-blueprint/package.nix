1d2bc5391716e14cbef3becdcbd545783f2e974b
Jason Yundt <jason@jasonyundt.email>
2025-07-28 10:18:05 -0400

    Update Nix package version number to 0.5.0

--- flake-blueprint/package.nix
+++ flake-blueprint/package.nix
@@ -6,10 +6,7 @@ let
 in
 pythonPackages.buildPythonApplication {
   pname = "jasons-pre-commit-hooks";
-  # TODO: This doesn’t match the version number used by the Python
-  # distribution package. I don’t know how to make this version number
-  # match that version number.
-  version = "0.dev${flake.lastModifiedDate}";
+  version = "0.5.0";
   src = flake;
   pyproject = true;
 
