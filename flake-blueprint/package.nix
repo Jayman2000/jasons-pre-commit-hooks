192bdf766cbd387fdbafc23551d096320a43f7ad
Jason Yundt <jason@jasonyundt.email>
2025-07-28 10:37:13 -0400

    Revert "Update Nix package version number to 0.5.0"
    This reverts commit 1d2bc5391716e14cbef3becdcbd545783f2e974b. The change
    from that commit only really makes sense for stable versions of Jason’s
    Pre-commit Hooks. I’m reverting it in the main branch because it doesn’t
    make sense for unstable versions of Jason’s Pre-commit Hooks.

--- flake-blueprint/package.nix
+++ flake-blueprint/package.nix
@@ -6,7 +6,10 @@ let
 in
 pythonPackages.buildPythonApplication {
   pname = "jasons-pre-commit-hooks";
-  version = "0.5.0";
+  # TODO: This doesn’t match the version number used by the Python
+  # distribution package. I don’t know how to make this version number
+  # match that version number.
+  version = "0.dev${flake.lastModifiedDate}";
   src = flake;
   pyproject = true;
 
